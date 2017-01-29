b3d3934b99f9c814b70d382864dc9c9cc901f29b
przempore <przempore@gmail.com>
2017-01-29 14:16:49 +0100

    base debugger done

--- deb.cpp
+++ deb.cpp
@@ -63,6 +63,53 @@ int main(int argc, char **argv)
             debug_ev.u.CreateThread.lpStartAddress);
         threads[debug_ev.dwThreadId] = debug_ev.u.CreateThread.hThread;
         break;
+      case EXCEPTION_DEBUG_EVENT:
+        cont_status = DBG_EXCEPTION_NOT_HANDLED;
+
+        printf("EXCEPTION, first chance: %u, code: %x, addres: %p\n",
+               debug_ev.u.Exception.dwFirstChance,
+               debug_ev.u.Exception.ExceptionRecord.ExceptionCode,
+               debug_ev.u.Exception.ExceptionRecord.ExceptionAddress);
+
+        if (!debug_ev.u.Exception.dwFirstChance) {
+          TerminateProcess(processes[debug_ev.dwProcessId], 0);
+        }
+        break;
+      case EXIT_PROCESS_DEBUG_EVENT:
+        printf("EXIT_PROCESS, pid: %u, tid: %u, exit code: %u\n",
+               debug_ev.dwProcessId, debug_ev.dwThreadId,
+               debug_ev.u.ExitProcess.dwExitCode);
+
+        CloseHandle(processes[debug_ev.dwProcessId]);
+        CloseHandle(threads[debug_ev.dwThreadId]);
+
+        processes.erase(debug_ev.dwProcessId);
+        threads.erase(debug_ev.dwThreadId);
+        break;
+      case EXIT_THREAD_DEBUG_EVENT:
+        printf("EXIT THREAD, tid: %u, exit code: %u\n",
+               debug_ev.dwThreadId, debug_ev.u.ExitThread.dwExitCode);
+
+        CloseHandle(threads[debug_ev.dwThreadId]);
+        threads.erase(debug_ev.dwThreadId);
+        break;
+      case LOAD_DLL_DEBUG_EVENT:
+        printf("LOAD_DLL, base address: %p\n", debug_ev.u.LoadDll.lpBaseOfDll);
+
+        if (debug_ev.u.LoadDll.hFile != NULL) {
+          CloseHandle(debug_ev.u.LoadDll.hFile);
+        }
+        break;
+      case UNLOAD_DLL_DEBUG_EVENT:
+        printf("UNLOAD_DLL, base address: %p\n", debug_ev.u.LoadDll.lpBaseOfDll);
+
+        break;
+      case OUTPUT_DEBUG_STRING_EVENT:
+        printf("OUTPUT_DEBUG_STRING\n");
+        break;
+      case RIP_EVENT:
+        printf("RIP_EVENT\n");
+        break;
     }
 
     ContinueDebugEvent(debug_ev.dwProcessId,
